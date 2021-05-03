e887d29efcc44b7f250258235d109be9b1357ae3
Sachin Kumar <sinha1abc@gmail.com>
2021-05-03 11:56:52 +0530

    hell

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const http = require("http");
 const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 9000;
 httpServer.listen(PORT, function () {
-	console.log(`Server Running on Port 9000`);
+	console.log(`Server Running on Port ${PORT}`);
 });
 //
 const { ExpressPeerServer } = require("peer");
